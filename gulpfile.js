af23db8ce598aa6f28f82ab6254557f82aa83230
Ruslan Makarenko <ruslan.makarenko@gmail.com>
2015-10-19 17:11:26 +0300

    added vendors

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,13 @@
-const gulp   = require('gulp');
-const tsc    = require('gulp-tsc');
-const server = require('gulp-server-livereload');
-const jade   = require('gulp-jade');
-const stylus = require('gulp-stylus');
-const tslint = require('gulp-tslint');
+const gulp     = require('gulp');
+const tsc      = require('gulp-tsc');
+const server   = require('gulp-server-livereload');
+const jade     = require('gulp-jade');
+const stylus   = require('gulp-stylus');
+const tslint   = require('gulp-tslint');
+const clean    = require('gulp-clean');  
+const concat   = require('gulp-concat');
+const filesize = require('gulp-filesize'); 
+
 
 var path = {
   src:'./src/**/',
@@ -17,16 +21,29 @@ gulp.task('webserver', function() {
     }));
 });
 
+gulp.task('clean-scripts', function () {
+  return gulp.src(path.dist + 'app/**/*.js', {read: false})
+    .pipe(clean());
+});
 
 //'json' prints stringified JSON to console.log.
 //'prose' prints short human-readable failures to console.log.
 //'verbose' prints longer human-readable failures to console.log.
 //'full' is like verbose, but displays full path to the file
 
+
+gulp.task('vendor', function() {  
+  return gulp.src('bower_components/**/*.min.js')
+    .pipe(concat('vendor.min.js'))
+    .pipe(gulp.dest(path.dist + 'vendor'));
+});
+
 gulp.task('tslint', function(){
   return gulp.src(path.src + '*.ts')
     .pipe(tslint())
-    .pipe(tslint.report('prose'));
+    .pipe(tslint.report('prose', {
+      emitError: false
+    }));
 });
 
 gulp.task('compile', function(){
